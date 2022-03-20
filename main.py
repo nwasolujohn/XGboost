59584c555285398cc4f8008dd12da298b7c7e4dc
nwasolujohn <97901914+nwasolujohn@users.noreply.github.com>
2022-03-20 14:01:05 +0100

    Create main.py
    Main body of the code

---
+++ main.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import fxnfile as fn 
+import seaborn as sns
+import matplotlib.pyplot as plt
+from xgboost import XGBRegressor as xgb
+from sklearn.model_selection import train_test_split as tt
